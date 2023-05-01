977b0461fc0a80b3c1a510e331baf3885a0beaac
Bene_Tesla <78994881+benetesla@users.noreply.github.com>
2023-05-01 12:17:36 -0300

    inicio backend
    Iniciando a contrução do backend da nossa aplicação

---
+++ api/src/test/java/com/sysmap/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sysmap.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
