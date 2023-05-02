fb11207f1680a0795b78169277609368d719192e
Bene_Tesla <78994881+benetesla@users.noreply.github.com>
2023-05-02 14:57:23 -0300

    feat: start codification authcontroller

---
+++ backend/src/main/java/com/sysmap/api/controller/AuthController.java
@@ -0,0 +1,10 @@
+packege com.sysmap.api.controller;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+    @Autowired
+    private AuthService authService;  
+    //transforma o objeto em json
+
+}
