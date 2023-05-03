15f3e043dbf4877ee9ad653d83be330221a7ef72
Bene_Tesla <78994881+benetesla@users.noreply.github.com>
2023-05-02 22:42:16 -0300

    feat: Adding Enable kafka to our app

--- backend/src/main/java/com/sysmap/api/ApiApplication.java
+++ backend/src/main/java/com/sysmap/api/ApiApplication.java
@@ -2,9 +2,10 @@ package com.sysmap.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.EnableKafka;
 @SpringBootApplication
+@EnableKafka
 public class ApiApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
 	}
